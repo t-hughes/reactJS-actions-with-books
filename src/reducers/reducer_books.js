73b38216756b68da39f2201232c2b658c62af68e
Talon Hughes <talon.hughes@gmail.com>
2016-12-06 21:01:55 -0700

    tiny book app to learn actions in reactJS

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default () => {
+    return [
+        {title: 'Enders Game', pages: 200 },
+        {title: 'Harry Potter', pages: 500 },
+        {title: 'Lord of the Rings', pages: 400 },
+        {title: 'The Hobbit', pages: 150}
+    ];
+};
